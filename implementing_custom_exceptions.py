b4b0d2b9ef5dcc22ad2efe7aaed7de5ee1c3ca2b
Ofek41 <ofekchen2006@gmail.com>
2024-11-30 17:35:17 +0200

    Added a new file that implements my 2 custom exceptions - invalid character in the expression and gibberish expression.

---
+++ implementing_custom_exceptions.py
@@ -0,0 +1,21 @@
+from custom_exceptions import *
+from main import OPERATORS
+# List that defines all the valid characters of the expression (without the operators):
+VALID_CHARACTERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')']
+def check_invalid_character(expression: str):
+    """
+    This function checks if the expression contains any invalid characters. if so, it raises the custom
+    exception with a matching message and the exact position and char.
+    """
+    index = 0
+    for char in expression:
+        if char not in VALID_CHARACTERS or char not in OPERATORS.keys():
+            raise InvalidCharacterInExpressionError(f"Invalid character '{char}' in position {index+1}.")
+    index+=1
+
+def check_gibberish_expression(expression: str):
+    """
+    This function checks if the expression is gibberish. If so, it raises the custom exception.
+    """
+    if all(char not in VALID_CHARACTERS and char not in OPERATORS.keys() for char in expression):
+        raise GibberishExpressionError("The expression you inserted is gibberish!")
