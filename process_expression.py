4803a45abbc783eabe315f5c5cd76d63ecdde20f
Ofek41 <ofekchen2006@gmail.com>
2024-12-02 13:15:51 +0200

    Added 2 new functions: one that gets the expression list and returns its string form, and one that >> gets the expression in infix notation and returns it in postfix notation.

--- process_expression.py
+++ process_expression.py
@@ -21,9 +21,12 @@ def check_full_validation_of_expression(expression: str) -> list:
         tokens = apply_tilde(tokens)
         # Validate operators and operands:
         validate_operators(tokens)
+        # Validate invalid characters or gibberish:
+        check_invalid_character(expression)
+        check_gibberish_expression(expression)
         return tokens
-    except Exception as e:
-        raise ValueError(f"Validation error in the expression: {e}")
+    except Exception as exc:
+        raise ValueError(f"Validation error in the expression: {exc}")
 
 def list_to_expression(tokens)->str:
     """
@@ -33,3 +36,33 @@ def list_to_expression(tokens)->str:
     for token in tokens:
         expression_string+=token
     return expression_string
+
+def infix_to_postfix(expression:str):
+    """
+    This function gets a string expression in infix form and returns its postfix form.
+    """
+    stack = [] # Stack that holds all the operators
+    postfix_expression = ""
+    for index in range(len(expression)):
+        char = expression[index]
+        # If the char is an operand:
+        if char not in OPERATORS.keys() and char not in "()":
+            postfix_expression+=char
+        elif char=='(': stack.append('(') # If the char is a parenthesis
+        elif char==')':
+            while stack[-1]!='(':
+                postfix_expression+=stack.pop()
+            stack.pop()
+        else: # If the char is an operator:
+            temp = OPERATORS.get(stack[-1]) # Get the operator on the top of the stack
+            while stack and OPERATORS.get(char).priority() <= temp.priority():
+                postfix_expression+=stack.pop()
+            stack.append(char)
+    while stack:
+        postfix_expression+=stack.pop()
+    return postfix_expression
+
+
+
+
+
