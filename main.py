6b9b42c8e09d545338252a12bf351d262d23ad49
Ofek41 <ofekchen2006@gmail.com>
2024-12-05 20:53:47 +0200

    Added the main function that gets the expression from the user and prints the result

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # Importing my modules:
 from operators import *
-# Global dictionary that holds all the operators:
-OPERATORS = {"+": Add(),
-             "-": Sub(),
-             "*": Multiply(),
-             "/": Division(),
-             "^": Power(),
-             "@": Average(),
-             "$": Maximum(),
-             "&": Minimum(),
-            "%": Modulo(),
-            "~":Negative(),
-            "!": Factorial()
-             }
+from custom_exceptions import *
+from implementing_custom_exceptions import *
+from parsing_expression import *
+from process_expression import *
+from operators_config import OPERATORS
+
+def main():
+    print("Hello! Welcome to the calculator!")
+    print("Please enter a math expression and you will get the result within seconds!")
+    expression = input()
+    postfix_expression = infix_to_postfix(expression)
+    result = postfix_calculation(postfix_expression)
+    print("The result is:", result)
+
+if __name__ == "__main__":
+    main()
