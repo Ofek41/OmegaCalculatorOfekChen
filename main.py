d9dfe0b11b23bc68e63084d52e1cfb0407ea6b10
Ofek41 <ofekchen2006@gmail.com>
2024-12-06 12:57:07 +0200

    Fixed factorial operator class and and fixed CLI in main function

--- main.py
+++ main.py
@@ -12,15 +12,20 @@ def main():
     print("If you would like to quit, just type exit!")
     while True:
         expression = input("Your expression: ")
-        if expression.strip().lower() == "exit":
+        if expression.strip().lower() == "exit": # Checking if the user inserted "exit"
             print("Goodbye, thank you for using!")
             break
         try:
+            # Converting the exp from infix to postfix:
             postfix_expression = infix_to_postfix(expression)
+            # Calculating the result based on postfix exp:
             result = postfix_calculation(postfix_expression)
+            # Checking if the result is int or float:
+            if isinstance(result, float) and result.is_integer():
+                result = int(result)
             print("The result is:", result)
-        except Exception as e:
-            print(f"Error: {e}")
+        except Exception as e: # If an error was raised, present it.
+            print("Error: ", e)
 
 if __name__ == "__main__":
     main()
