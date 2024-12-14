38dbb0eddb372234f2d01935fed4fc700315efca
Ofek41 <ofekchen2006@gmail.com>
2024-12-14 17:02:30 +0200

    Fixed validation of decimal point

--- implementing_custom_exceptions.py
+++ implementing_custom_exceptions.py
@@ -2,20 +2,18 @@ from custom_exceptions import *
 from operators_config import OPERATORS
 
 # List that defines all the valid characters of the expression (without the operators):
-VALID_CHARACTERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')']
+VALID_CHARACTERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')', '.']
 def check_invalid_character(expression: str):
     """
-    This function checks if the expression contains any invalid characters. If so, it raises the custom
-    exception with a matching message and the exact position and char.
+    Checks if the expression contains invalid characters.
+    If so, raises a single exception with all invalid character positions.
     """
-    index = 0
-    invalid_positions = []
-    for char in expression:
-        if char not in VALID_CHARACTERS and char not in OPERATORS.keys():
-            invalid_positions.append(index)
-        index += 1
-    if len(invalid_positions)!=0:
-        raise InvalidCharacterInExpressionError(f"Invalid character in positions: {invalid_positions}")
+    invalid_positions = [
+        index + 1 for index, char in enumerate(expression)
+        if char not in VALID_CHARACTERS and char not in OPERATORS.keys()
+    ]
+    if invalid_positions:
+        raise InvalidCharacterInExpressionError(f"Invalid characters at positions: {invalid_positions}")
 
 def check_gibberish_expression(expression: str):
     """
