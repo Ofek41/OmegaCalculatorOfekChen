15bdf9bfc5e420ae76e6861a43921f98bdb240d5
Ofek41 <ofekchen2006@gmail.com>
2024-12-22 19:57:32 +0200

    Added final EOF and KeyboardInterrupt exceptions for main

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def main():
     print("Please enter a math expression and you will get the result within seconds!")
     print("If you would like to quit, just type quit!")
     while True:
-        expression = input("Your expression: ")
-        if expression.strip().lower() == "quit":
-            print("Goodbye, thank you for using!")
-            break
         try:
+            expression = input("Your expression: ")
+            if expression.strip().lower() == "quit":
+                print("Goodbye, thank you for using!")
+                break
             tokens = check_full_validation_of_expression(expression)
             postfix_expression = infix_to_postfix(tokens)
             result = postfix_calculation(postfix_expression)
@@ -18,7 +18,13 @@ def main():
                 result = int(result)
             print("The result is:", result)
         except Exception as e:
-            print("Error:", e)
+            if isinstance(e, EOFError):
+                print("Ctrl + Z is not supported")
+            else:
+                print("Error:", e)
+        except KeyboardInterrupt:
+            print("\nProgram has been closed")
+            break
 
 if __name__ == "__main__":
     main()
